3ca35ee44bdf3f6f6aed9466afe5f83731a1efd4
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2024-03-24 01:34:10 +0000

    Refactoring configuration code

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'moushtario'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/MyDB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
